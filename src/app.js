7e96668ceefb99a7bb6d7c4be3f0b2f4e039343c
Krishna Kushwaha <120515502+krishna9788@users.noreply.github.com>
2023-04-19 22:47:43 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ app.use(express.json());
 // Return the response as {message : , status: }
 
 module.exports = app;
+const express = require('express');
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+// Write a GET Request to get the next number from the input 'num'.
+// Endpoint : /api/get-next-num
+// Return the response as {message : , status: }
+app.get('/api/get-next-num', (req, res) => {
+    res.status(200).json({ "message": req.body.num + 1, "status": "success" })
+})
+
+module.exports = app;
