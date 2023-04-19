ab544afddc34ccf995b88ccde59ae096d5473617
Krishna Kushwaha <120515502+krishna9788@users.noreply.github.com>
2023-04-19 23:24:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,5 @@
 const express = require('express');
-const app = express();
-
-//Middlewares
-app.use(express.json());
-
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
-
-module.exports = app;
-const express = require('express');
-const app = express();
-
-//Middlewares
-app.use(express.json());
-
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
-app.get('/api/get-next-num', (req, res) => {
-    res.status(200).json({ "message": req.body.num + 1, "status": "success" })
-})
-
-module.exports = app;
-const express = require('express');
+constconst express = require('express');
 const app = express();
 
 //Middlewares
